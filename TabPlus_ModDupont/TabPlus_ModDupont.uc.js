87ccf10b809d3035d6fffa4de3245c98b0bb1f3c
玄清介 <dupontjoy@163.com>
2015-05-23 08:01:07 +0800

    05.23

---
+++ TabPlus_ModDupont/TabPlus_ModDupont.uc.js
@@ -0,0 +1,186 @@
+// ==UserScript==
+// @description  Tab Plus 标签页增强
+// @include      chrome://browser/content/browser.xul
+
+// 2015.04.21 14:00 加入滾輪切換，自動聚焦
+// 2015.04.02 10:00 精簡
+// 2015.03.19 16:00 open_in_new_tab更新到GOLF-AT 2.1.20150305版
+// 2015.01.23 21:00 新标签打开『查看图片』
+// 2014.07.26 點擊頁面恢復原來的地址
+// 2014.05.14 修改激活左侧标签
+// ==/UserScript==
+
+//地址栏、搜索栏、书签菜单、书签工具栏、历史菜单、主页按钮
+
+(function() {
+    try {
+        eval('isBlankPageURL = ' + isBlankPageURL.toString().replace(
+            'return ', '$&aURL=="" || aURL=="about:privatebrowsing" '
+            +'|| '));
+    }catch(e){}
+
+    /* 在当前标签页打开 Bookmarklet */
+    try {
+        eval('openLinkIn = ' + openLinkIn.toString().replace(
+            'if (where == "save")', 'if (url.match(/^javascript:/))\n'+
+            '    where = "current";\n  $&'));
+    }catch(e){}
+
+    /* open bookmark/history in new tab */
+    try {
+        eval("whereToOpenLink = " + whereToOpenLink.toString().replace(
+            'return "window";',"$&\n\n  var Class = e.target?e.target."
+            +"getAttribute('class') : null;\n  try {\n    if (Class=='"
+            +"')\n      Class=e.target.parentNode.getAttribute('class'"
+            +");\n  }catch(e) {}\n  if ((!isBlankPageURL(gBrowser.curr"
+            +"entURI.spec) || gBrowser.webProgress.isLoadingDocument) "
+            +"&& Class && (Class.indexOf('bookmark-item')>=0 || Class."
+            +"indexOf('placesTree')>=0 || Class=='subviewbutton') && !"
+            +"IsInSideBar(e.target))\n    return 'tab';"));
+    }catch(e){}
+
+    /* bookmark/history on sidebar/place-manager */
+    try {
+        eval("PlacesUIUtils.openNodeWithEvent = " + PlacesUIUtils.
+            openNodeWithEvent.toString().replace("window.whereToOpenLink"
+            , "whereToOpenLink"));
+    }catch(e){}
+    
+    /* open url in new tab */
+    try {
+        try { // firefox 3.0.*
+            eval("BrowserLoadURL = "+ BrowserLoadURL.toString().replace(
+                /if \(aTriggeringEvent instanceof MouseEvent\) {/,
+                "_LoadURL(aTriggeringEvent, aPostData); return; $&"));
+        }
+        catch(e) { // firefox 3.1+
+            var urlbar = document.getElementById("urlbar");
+            eval("urlbar.handleCommand="+ urlbar.handleCommand.toString(
+                ).replace("aTriggeringEvent.altKey && ", "").replace(
+                "altEnter && !isTabEmpty","!isMouseEvent && !isTabEmpty"
+                ));
+        }
+    }catch(e){}
+
+    /* open home in new tab */
+    try {
+        eval("BrowserGoHome = " + BrowserGoHome.toString().replace(
+            /switch \(where\) {/, "where = (gBrowser.currentURI.spec!="
+            +"'about:blank' || gBrowser.webProgress.isLoadingDocument"+
+            ") ? 'tab' : 'current'; $&")); 
+    }catch(e){}
+
+    /* open search in new tab */
+    try {
+        var searchbar = document.getElementById("searchbar");
+        eval("searchbar.handleSearchCommand="+searchbar.handleSearchCommand.
+            toString().replace(/this.doSearch\(textValue,/,
+            "if (!gBrowser.webProgress.isLoadingDocument&&\n\t\tisBlankPage"
+            +"URL(gBrowser.currentURI.spec))\n\t\twhere='current';\n\telse"+
+            "\n\t\twhere='tab';\n\t$&"));
+    }catch(e){}
+
+})();
+
+function _LoadURL(aTriggeringEvent, aPostData)
+{
+    var where = (gBrowser.currentURI.spec!='about:blank' ||
+        gBrowser.webProgress.isLoadingDocument) ? 'tab' :
+        'current';
+    if (gURLBar.value!='') openUILinkIn(gURLBar.value, where);
+    return true;
+}
+
+function IsInSideBar(target)
+{
+    if (!target) return false;
+    try {
+        var node = target._placesNode;
+    }catch(e) { node = null; }
+    try {
+        if (!node && (target.parentNode.id=='placeContent'
+            || target.parentNode.id=='bookmarks-view'))
+            node = target.parentNode.selectedNode;
+        return node && PlacesUtils.nodeIsBookmark(node) &&
+            PlacesUtils.annotations.itemHasAnnotation(node
+            .itemId,PlacesUIUtils.LOAD_IN_SIDEBAR_ANNO);
+    }catch(e) { return false; }
+}
+
+/*=====以下爲另外收集整合的腳本=====*/
+
+//紧邻当前标签新建标签页
+(function() {
+    try {
+        if(!gBrowser) return;
+    }catch(e) {
+        return;
+    }
+    
+    gBrowser.tabContainer.addEventListener("TabOpen", tabOpenHandler, false);
+
+    function tabOpenHandler(event) {
+        var tab = event.target;
+        gBrowser.moveTabTo(tab, gBrowser.mCurrentTab._tPos + 1);
+    }
+
+})();
+
+//标签页关闭后激活左侧标签页
+(function() {
+    try {
+        if(!gBrowser) return;
+    }catch(e) {
+        return;
+    }
+
+    gBrowser.tabContainer.addEventListener("TabClose", tabCloseHandler, false);
+
+    function tabCloseHandler(event) {
+        var tab = event.target;
+                // 如果是因下载而产生的空白页
+                if (tab.linkedBrowser.contentDocument.URL == 'about:blank') return;
+                if (tab._tPos <= gBrowser.mTabContainer.selectedIndex){
+                        if (tab.previousSibling) {
+                                gBrowser.mTabContainer.selectedIndex--;
+                        }
+                }
+    }
+
+})();
+
+//點擊頁面恢復原來的地址
+gBrowser.addEventListener("DOMWindowCreated", function () {
+window.content.document.addEventListener("click", function (e) {
+document.getElementById("urlbar").handleRevert();
+}, false);
+}, false);
+    
+//新标签打开『查看图片』
+location == "chrome://browser/content/browser.xul" && document.querySelector("#context-viewimage").setAttribute("oncommand", 'openUILinkIn(gContextMenu.imageURL,"tab")') & document.querySelector("#context-viewbgimage").setAttribute("oncommand", 'openUILinkIn(gContextMenu.bgImageURL,"tab")')
+
+//滚轮切换标签
+    gBrowser.mTabContainer.addEventListener("DOMMouseScroll", function(event){
+        this.advanceSelectedTab(event.detail > 0 ? +1 : -1, true);
+    }, true);
+
+/*
+//鼠标停留标签自动聚焦
+     (document.getElementById("tabbrowser-tabs") || gBrowser.mTabBox).addEventListener('mouseover',
+    function self(e) {
+        if ((self.target = e.target).localName === 'tab') {
+            if (!self.timeoutID) {
+                this.addEventListener('mouseout',
+                function() {
+                    clearTimeout(self.timeoutID);
+                },
+                false);
+            }
+            self.timeoutID = setTimeout(function() {
+                gBrowser.selectedTab = self.target;
+            },
+            50);
+        }
+    },
+    false); 
+*/
---
+++ setRelativeEditPath/setRelativeEditPath.uc.js
@@ -0,0 +1,42 @@
+
+//2015.04.25 Cache相對位置
+//2015.03.29 Software分離，相應Path修改
+//2015.03.06 移动文件夾
+//2014.08.27 添加油猴Greasemonkey外部编辑器
+location == 'chrome://browser/content/browser.xul' && (function(){
+
+    var PATH1 = Services.dirsvc.get("ProfD", Ci.nsILocalFile).path + "..\\..\\Software\\Notepad2\\Notepad2.exe";
+    
+    var PATH2 = Services.dirsvc.get("ProfD", Ci.nsILocalFile).path + "..\\..\\..\\FX-Cache";
+
+    var handleRelativePath = function (path) {
+        if (path) {
+        path = path.replace(/\//g, '\\').toLocaleLowerCase();
+        var ProfD = Cc['@mozilla.org/file/directory_service;1'].getService(Ci.nsIProperties)
+                .get("ProfD", Ci.nsILocalFile).path;
+        if (/^(\\)/.test(path)) {
+            return ProfD + path;
+        } else {
+            return path;
+            }
+        }
+    };
+    
+//Firefox自带全局编辑器和Greasemonkey編輯器
+    var file = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsILocalFile);
+    file.initWithPath(handleRelativePath(PATH1));
+    if (file.exists()) {
+        gPrefService.setCharPref('view_source.editor.path', file.path);
+        gPrefService.setCharPref('extensions.greasemonkey.editor', file.path);
+    }
+    
+    
+//Cache位置
+    var file = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsILocalFile);
+    file.initWithPath(handleRelativePath(PATH2));
+    if (file.exists()) {
+        gPrefService.setCharPref('browser.cache.disk.parent_directory', file.path);
+        gPrefService.setCharPref('browser.cache.offline.parent_directory', file.path);
+    }
+    
+})()
