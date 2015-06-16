ee926dd1c053b326cfa57240197958141e9a5c9b
玄清介 <dupontjoy@163.com>
2015-06-16 08:21:20 +0800

    06.16

--- singletonFox/singletonFox.uc.js
+++ singletonFox/singletonFox.uc.js
@@ -19,4 +19,60 @@
 // ver 1.2 @ 2008-7-26
 //  Prevent Firefox from opening a new window by clicking a link when pressing SHIFT.
 
-(function() {var CW=function(){var rf=function(){var f3=typeof PlacesController==='function';window.OpenBrowserWindow=window.BrowserOpenTab;eval('window.handleLinkClick='+window.handleLinkClick.toString().replace('openNewWindowWith(href, doc'+(f3?'':'URL')+', null, false);','openNewTabWith(href, doc'+(f3?'':'URL')+', null, event, false);'));eval('nsContextMenu.prototype.openLink='+nsContextMenu.prototype.openLinkInTab);eval('nsBrowserAccess.prototype.openURI='+nsBrowserAccess.prototype.openURI.toString().replace('switch (aWhere) {','aWhere= Ci.nsIBrowserDOMWindow.OPEN_NEWTAB;\n$&'));if(f3)eval('PlacesController.prototype.doCommand='+PlacesController.prototype.doCommand.toString().replace('"window"','"tab"'));else eval('BookmarksController.doCommand='+BookmarksController.doCommand.toString().replace('"window"','"tab"'))},op=function(){var $=Cc['@mozilla.org/preferences-service;1'].getService(Ci.nsIPrefBranch);$.setIntPref('browser.link.open_newwindow',3);$.setIntPref('browser.link.open_newwindow.restriction',0);$.setIntPref('browser.link.open_external',3)},gm=function($){return(gm=function(){return $})()},ib=function($){return $.mTabs.length===1&&$.currentURI.spec==='about:blank'&&$.selectedBrowser.sessionHistory.count<2&&!$.selectedTab.hasAttribute('busy')&&!$.selectedBrowser.contentDocument.body.hasChildNodes()},il=function($){return $.currentURI.spec!=='about:blank'||$.selectedTab.getAttribute('busy')===''||$.selectedBrowser.contentDocument.body.hasChildNodes()};return{nt:function(){var _=Cc['@mozilla.org/appshell/window-mediator;1'].getService(Ci.nsIWindowMediator).getEnumerator('navigator:browser');if(_.hasMoreElements())while(_.hasMoreElements()){var B=_.getNext(),A=gm(B);if(B!==A){var $=B.getBrowser();if(!ib($))(function(){if(!il($))setTimeout(arguments.callee,0);else{var _=Cc['@mozilla.org/browser/sessionstore;1'].getService(Ci.nsISessionStore);_.setWindowState(A,_.getWindowState(B),false);A.focus();B.BrowserTryToCloseWindow()}})();else B.BrowserTryToCloseWindow()}else{rf();op()}}return false}}}();CW.nt()})();
+(function() {
+  var CW = function() {
+    var rf = function() {
+      var f3 = typeof PlacesController === 'function';
+      window.OpenBrowserWindow = window.BrowserOpenTab;
+      eval('window.handleLinkClick=' + window.handleLinkClick.toString().replace('openNewWindowWith(href, doc' + (f3 ? '': 'URL') + ', null, false);', 'openNewTabWith(href, doc' + (f3 ? '': 'URL') + ', null, event, false);'));
+      eval('nsContextMenu.prototype.openLink=' + nsContextMenu.prototype.openLinkInTab);
+      eval('nsBrowserAccess.prototype.openURI=' + nsBrowserAccess.prototype.openURI.toString().replace('switch (aWhere) {', 'aWhere= Ci.nsIBrowserDOMWindow.OPEN_NEWTAB;\n$&'));
+      if (f3) eval('PlacesController.prototype.doCommand=' + PlacesController.prototype.doCommand.toString().replace('"window"', '"tab"'));
+      else eval('BookmarksController.doCommand=' + BookmarksController.doCommand.toString().replace('"window"', '"tab"'))
+    },
+    op = function() {
+      var $ = Cc['@mozilla.org/preferences-service;1'].getService(Ci.nsIPrefBranch);
+      $.setIntPref('browser.link.open_newwindow', 3);
+      $.setIntPref('browser.link.open_newwindow.restriction', 0);
+      $.setIntPref('browser.link.open_external', 3)
+    },
+    gm = function($) {
+      return (gm = function() {
+        return $
+      })()
+    },
+    ib = function($) {
+      return $.mTabs.length === 1 && $.currentURI.spec === 'about:blank' && $.selectedBrowser.sessionHistory.count < 2 && !$.selectedTab.hasAttribute('busy') && !$.selectedBrowser.contentDocument.body.hasChildNodes()
+    },
+    il = function($) {
+      return $.currentURI.spec !== 'about:blank' || $.selectedTab.getAttribute('busy') === '' || $.selectedBrowser.contentDocument.body.hasChildNodes()
+    };
+    return {
+      nt: function() {
+        var _ = Cc['@mozilla.org/appshell/window-mediator;1'].getService(Ci.nsIWindowMediator).getEnumerator('navigator:browser');
+        if (_.hasMoreElements()) while (_.hasMoreElements()) {
+          var B = _.getNext(),
+          A = gm(B);
+          if (B !== A) {
+            var $ = B.getBrowser();
+            if (!ib($))(function() {
+              if (!il($)) setTimeout(arguments.callee, 0);
+              else {
+                var _ = Cc['@mozilla.org/browser/sessionstore;1'].getService(Ci.nsISessionStore);
+                _.setWindowState(A, _.getWindowState(B), false);
+                A.focus();
+                B.BrowserTryToCloseWindow()
+              }
+            })();
+            else B.BrowserTryToCloseWindow()
+          } else {
+            rf();
+            op()
+          }
+        }
+        return false
+      }
+    }
+  } ();
+  CW.nt()
+})();
