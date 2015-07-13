e4de4ccd61a618bcfdf527e3a485417aa2e7a5b8
玄清介 <dupontjoy@163.com>
2015-07-13 18:41:08 +0800

    07.14

---
+++ BackupProfiles_7z/Backup_7z.bat
@@ -0,0 +1,582 @@
+
+::2015.07.13 18:00  4合1整合
+
+@echo off
+Title 備份批處理4合1版 by Cing
+
+:menu
+MODE con: COLS=80 LINES=25
+ECHO.
+ECHO =============================================================================
+ECHO                        Windows 8.1 Update 优化辅助工具                           
+ECHO    #+++++++++++++++++++++++++++++++++#+++++++++++++++++++++++++++++++++++#
+ECHO    # 01、備份Firefox配置文件夾    # 03、備份Plugins和Software文件夾      #
+ECHO    # 02、CingFox完整包制作        # 04、提取Flash32位插件                #
+ECHO    #+++++++++++++++++++++++++++++++++#+++++++++++++++++++++++++++++++++++#
+ECHO =============================================================================
+
+set /p a=.                  请输入操作序号并回车（例如07）：
+if %a%==01 goto Profiles
+if %a%==02 goto CingFox
+if %a%==03 goto Plugins-n-Software
+if %a%==04 goto Flash32
+goto cho
+
+:Profiles
+CLS
+MODE con: COLS=40 LINES=15
+ECHO.
+ECHO.
+ECHO    **********************************
+ECHO.
+ECHO              禁用索引服务
+ECHO.
+ECHO                1.执行
+ECHO.
+ECHO                2.返回
+ECHO.
+ECHO    **********************************
+ECHO.
+ECHO.
+Choice /C 12 /N /M 选择（1、2）：
+If ErrorLevel 1 If Not ErrorLevel 2 Goto Profiles-1
+If ErrorLevel 2 If Not ErrorLevel 3 Goto menu
+
+:Profiles-1
+MODE con: COLS=80 LINES=25
+Title 備份Firefox配置文件夾 by Cing
+echo.
+echo    *** 備份Firefox配置文件夾 ***
+echo.
+echo ============================================================
+echo    **注意：
+echo.
+echo    1. 需要關閉Firefox程序，請保存必要的資料!
+echo.
+echo    2. 備份完成後，按任意鍵重啟Firefox
+echo.
+echo    By Cing(Dupontjoy)
+echo.
+echo    按任意键继续……
+echo =============================================================
+pause>nul
+cls
+
+rem 設置備份路徑以及臨時文件夾
+taskkill /im firefox.exe
+@echo 關閉火狐瀏覽器后自動開始備份……
+cd /d %~dp0
+::从批处理所在位置到配置文件夹（Profiles），共跨了3层
+set BackDir=..\..\..
+set TempFolder=..\..\..\Temp\Profiles
+
+taskkill /im firefox.exe
+
+rem 复制目标文件到臨時文件夾
+
+::以下是文件夾
+::adblockplus：ABP規則備份。
+xcopy "%BackDir%\adblockplus" %TempFolder%\adblockplus\  /s /y /i
+::autoproxy：Autoproxy規則備份。
+xcopy "%BackDir%\autoproxy" %TempFolder%\autoproxy\  /s /y /i
+::chrome：UC腳本。
+xcopy "%BackDir%\chrome" %TempFolder%\chrome\  /s /y /i
+::extensions：安裝的擴展。
+xcopy "%BackDir%\extensions" %TempFolder%\extensions\ /s /y /i
+::extension-data：uBlock的數據文件，包含設置。
+xcopy "%BackDir%\extension-data" %TempFolder%\extension-data\ /s /y /i
+::gm_scripts：安裝的油猴腳本。
+xcopy "%BackDir%\gm_scripts" %TempFolder%\gm_scripts\ /s /y /i
+::Plugins：便携版插件。
+xcopy "%BackDir%\Plugins" %TempFolder%\Plugins\ /s /y /i
+ 
+::需要刪除的项
+del %TempFolder%\chrome\UserScriptLoader\require\  /s /q
+del %TempFolder%\extensions\userChromeJS@mozdev.org\content\myNewTab\bingImg\  /s /q
+del %TempFolder%\extensions\inspector@mozilla.org\chrome\inspector\locale\de\  /s /q
+del %TempFolder%\extensions\inspector@mozilla.org\chrome\inspector\locale\en-GB\  /s /q
+del %TempFolder%\extensions\inspector@mozilla.org\chrome\inspector\locale\pl\  /s /q
+del %TempFolder%\extensions\inspector@mozilla.org\chrome\inspector\locale\ru\  /s /q
+del %TempFolder%\extensions\inspector@mozilla.org\chrome\inspector\locale\sk\  /s /q
+del %TempFolder%\extensions\support@lastpass.com\platform\Darwin\  /s /q
+del %TempFolder%\extensions\support@lastpass.com\platform\Darwin_x86_64-gcc3\  /s /q
+del %TempFolder%\extensions\support@lastpass.com\platform\Linux_x86_64-gcc3\  /s /q
+del %TempFolder%\extensions\support@lastpass.com\platform\Linux_x86-gcc3\  /s /q
+del %TempFolder%\gm_scripts\picviewer_CE.db  /s /q
+del %TempFolder%\gm_scripts\picviewer_CE.db-shm  /s /q
+del %TempFolder%\gm_scripts\picviewer_CE.db-wal  /s /q
+del %TempFolder%\gm_scripts\YouTube_Auto_Buffer_&_Auto_HD.db  /s /q
+del %TempFolder%\gm_scripts\繞過站點等待、識別碼及登錄.db  /s /q
+del %TempFolder%\gm_scripts\跳过网站等待、验证码及登录.db  /s /q
+del %TempFolder%\gm_scripts\繞過站點等待、識別碼及登錄.db-shm  /s /q
+del %TempFolder%\gm_scripts\跳过网站等待、验证码及登录.db-wal  /s /q
+
+::以下是文件
+::bookmarks.html：自動导出的书签備份。
+xcopy "%BackDir%\bookmarks.html" %TempFolder%\ /y
+::cert_override.txt：储存使用者指定的例外证书(certification exceptions)。
+xcopy "%BackDir%\cert_override.txt" %TempFolder%\ /y
+::cert8.db：安全证书。
+xcopy "%BackDir%\cert8.db" %TempFolder%\ /y
+::FlashGot.exe：FlashGot的下载工具。
+xcopy "%BackDir%\FlashGot.exe" %TempFolder%\ /y
+::foxyproxy.xml：FoxyProxy的設置及网址列表備份。
+::xcopy "%BackDir%\foxyproxy.xml" %TempFolder%\ /y
+::localstore.rdf：工具列与视窗大小／位置的設定，有時刪掉可以解决一些介面上的问题。
+xcopy "%BackDir%\localstore.rdf" %TempFolder%\ /y
+::mimeTypes.rdf：下载特定类型的档案時要执行的動作。 可刪掉来还原原来下载的設定。
+xcopy "%BackDir%\mimeTypes.rdf" %TempFolder%\ /y
+::MyFirefox.7z：用於官方FX的便携設置。
+xcopy "%BackDir%\MyFirefox.7z" %TempFolder%\ /y
+::patternSubscriptions.json：FoxyProxy的訂閱列表設置。
+::xcopy "%BackDir%\patternSubscriptions.json" %TempFolder%\ /y
+::permissions.sqlite：存放特定网站是否可存取密码、cookies、弹出视窗、图片载入与附加元件……等权限的资料库。
+xcopy "%BackDir%\permissions.sqlite" %TempFolder%\ /y
+::persdict.dat：个人的拼字字典。
+xcopy "%BackDir%\persdict.dat" %TempFolder%\ /y
+::pluginreg.dat：用于plugin的MIME types。
+xcopy "%BackDir%\pluginreg.dat" %TempFolder%\ /y
+::Portable.7z：PCXFirefox的便携設置。
+xcopy "%BackDir%\Portable.7z" %TempFolder%\ /y
+::prefs.js：About:config中儲存的設定。
+xcopy "%BackDir%\prefs.js" %TempFolder%\ /y
+::readme.txt：个人配置修改说明。
+xcopy "%BackDir%\readme.txt" %TempFolder%\ /y
+::stylish.sqlite：Stylish样式數据库。
+xcopy "%BackDir%\stylish.sqlite" %TempFolder%\ /y
+::user.js：使用者自订的設定，在这里的設定覆盖prefs.js的設定。
+xcopy "%BackDir%\user.js" %TempFolder%\ /y
+::xulstore.json：界面的一些状态。
+xcopy "%BackDir%\xulstore.json" %TempFolder%\ /y
+
+::讀取版本號和日期及時間
+::从批处理所在位置到Firefox程序文件夹（firefox），共跨了4层
+for /f "usebackq eol=; tokens=1,2 delims==" %%i in ("..\..\..\..\Firefox\application.ini")do (if %%i==Version set ver=%%j)
+::設置備份文件路徑以及文件名
+
+::完整日期和時間
+set tm1=%time:~0,2%
+set tm2=%time:~3,2%
+set tm3=%time:~6,2%
+set tm4=%time:~0,8%
+set da1=%date:~0,4%
+set da2=%date:~5,2%
+set da3=%date:~8,2%
+set ArchiveName=D:\Profiles_%da1%%da2%%da3%-%tm1%%tm2%%tm3%_%ver%.7z
+
+::小時數小于10点時的修正
+set /a tm1=%time:~0,2%*1
+if %tm1% LSS 10 set tm1=0%tm1%
+set ArchiveName=D:\Profiles_%da1%%da2%%da3%-%tm1%%tm2%%tm3%_%ver%.7z
+
+rem 開始備份
+7z.exe u -up1q3r2x2y2z2w2 %ArchiveName% "%TempFolder%"
+@echo 備份完成！并刪除臨時文件夾！
+rd "%TempFolder%" /s/q
+
+ECHO.&ECHO.Firefox配置已打包完成，請按任意鍵 重啟Firefox 並退出！&PAUSE >NUL 2>NUL
+
+@ping 127.0.0.1>nul
+@start ..\..\..\..\Firefox\firefox.exe
+
+Goto end
+
+:CingFox
+CLS
+MODE con: COLS=40 LINES=15
+ECHO.
+ECHO.
+ECHO    **********************************
+ECHO.
+ECHO              禁用索引服务
+ECHO.
+ECHO                1.执行
+ECHO.
+ECHO                2.返回
+ECHO.
+ECHO    **********************************
+ECHO.
+ECHO.
+Choice /C 12 /N /M 选择（1、2）：
+If ErrorLevel 1 If Not ErrorLevel 2 Goto CingFox-1
+If ErrorLevel 2 If Not ErrorLevel 3 Goto menu
+
+:CingFox-1
+MODE con: COLS=80 LINES=25
+Title CingFox完整包制作 by Cing
+echo.
+echo    *** CingFox完整包制作 ***
+echo.
+echo ============================================================
+echo    **注意：
+echo.
+echo    1. 需要關閉Firefox程序，請保存必要的資料!
+echo.
+echo    2. 備份完成後，按任意鍵重啟Firefox
+echo.
+echo    By Cing(Dupontjoy)
+echo.
+echo    按任意键继续……
+echo =============================================================
+pause>nul
+cls
+
+rem 設置備份路徑以及臨時文件夾
+taskkill /im firefox.exe
+@echo 關閉火狐瀏覽器后自動開始備份……
+cd /d %~dp0
+::从批处理所在位置到Mozilla Firefox大文件夾，共跨了4层
+set BackDir=..\..\..\..
+set TempFolder=..\..\..\..\CingFox
+
+taskkill /im firefox.exe
+
+@echo 備份firefox文件夾================================
+::firefox：pcxFirefox主程序
+xcopy "%BackDir%\firefox" %TempFolder%\firefox\  /s /y /i
+
+@echo 備份Plugins文件夾================================
+::Plugins：外置便携插件
+xcopy "%BackDir%\Plugins" %TempFolder%\Plugins\  /s /y /i
+
+::需要刪除的项
+del %TempFolder%\Plugins\sumatrapdfcache\  /s /q
+
+@echo 備份Software文件夾================================
+::Software：常用軟件
+xcopy "%BackDir%\Software" %TempFolder%\Software\  /s /y /i
+
+::需要刪除的项
+del %TempFolder%\Software\GFW\goagent\local\proxy.user.ini  /s /q
+del %TempFolder%\Software\GFW\Shadowsocks\gui-config.json  /s /q
+del %TempFolder%\Software\GFW\psiphon\psiphon3.exe.orig  /s /q 
+del %TempFolder%\Software\GFW\GoGoTester\gogo_cache  /s /q 
+
+@echo 備份Profiles文件夾================================
+rem 复制目标文件到臨時文件夾
+
+::以下是文件夾
+::adblockplus：ABP規則備份。
+xcopy "%BackDir%\Profiles\adblockplus" %TempFolder%\Profiles\adblockplus\  /s /y /i
+::autoproxy：Autoproxy規則備份。
+xcopy "%BackDir%\Profiles\autoproxy" %TempFolder%\Profiles\autoproxy\  /s /y /i
+::chrome：UC腳本。
+xcopy "%BackDir%\Profiles\chrome" %TempFolder%\Profiles\chrome\  /s /y /i
+::extensions：安裝的擴展。
+xcopy "%BackDir%\Profiles\extensions" %TempFolder%\Profiles\extensions\ /s /y /i
+::extension-data：uBlock的數據文件，包含設置。
+xcopy "%BackDir%\Profiles\extension-data" %TempFolder%\Profiles\extension-data\ /s /y /i
+::gm_scripts：安裝的油猴腳本。
+xcopy "%BackDir%\Profiles\gm_scripts" %TempFolder%\Profiles\gm_scripts\ /s /y /i
+::Plugins：便携版插件。
+xcopy "%BackDir%\Profiles\Plugins" %TempFolder%\Profiles\Plugins\ /s /y /i
+
+::需要刪除的项
+del %TempFolder%\Profiles\chrome\UserScriptLoader\require\  /s /q
+del %TempFolder%\Profiles\extensions\userChromeJS@mozdev.org\content\myNewTab\bingImg\  /s /q
+del %TempFolder%\Profiles\extensions\inspector@mozilla.org\chrome\inspector\locale\de\  /s /q
+del %TempFolder%\Profiles\extensions\inspector@mozilla.org\chrome\inspector\locale\en-GB\  /s /q
+del %TempFolder%\Profiles\extensions\inspector@mozilla.org\chrome\inspector\locale\pl\  /s /q
+del %TempFolder%\Profiles\extensions\inspector@mozilla.org\chrome\inspector\locale\ru\  /s /q
+del %TempFolder%\Profiles\extensions\inspector@mozilla.org\chrome\inspector\locale\sk\  /s /q
+del %TempFolder%\Profiles\extensions\support@lastpass.com\platform\Darwin\  /s /q
+del %TempFolder%\Profiles\extensions\support@lastpass.com\platform\Darwin_x86_64-gcc3\  /s /q
+del %TempFolder%\Profiles\extensions\support@lastpass.com\platform\Linux_x86_64-gcc3\  /s /q
+del %TempFolder%\Profiles\extensions\support@lastpass.com\platform\Linux_x86-gcc3\  /s /q
+del %TempFolder%\Profiles\gm_scripts\picviewer_CE.db  /s /q
+del %TempFolder%\Profiles\gm_scripts\picviewer_CE.db-shm  /s /q
+del %TempFolder%\Profiles\gm_scripts\picviewer_CE.db-wal  /s /q
+del %TempFolder%\Profiles\gm_scripts\YouTube_Auto_Buffer_&_Auto_HD.db  /s /q
+del %TempFolder%\Profiles\gm_scripts\繞過站點等待、識別碼及登錄.db  /s /q
+del %TempFolder%\Profiles\gm_scripts\跳过网站等待、验证码及登录.db  /s /q
+del %TempFolder%\Profiles\gm_scripts\繞過站點等待、識別碼及登錄.db-shm  /s /q
+del %TempFolder%\Profiles\gm_scripts\跳过网站等待、验证码及登录.db-wal  /s /q
+
+::以下是文件
+::cert_override.txt：储存使用者指定的例外证书(certification exceptions)。
+xcopy "%BackDir%\Profiles\cert_override.txt" %TempFolder%\Profiles\ /y
+::cert8.db：安全证书。
+xcopy "%BackDir%\Profiles\cert8.db" %TempFolder%\Profiles\ /y
+::FlashGot.exe：FlashGot的下载工具。
+xcopy "%BackDir%\Profiles\FlashGot.exe" %TempFolder%\Profiles\ /y
+::foxyproxy.xml：FoxyProxy的設置及网址列表備份。
+::xcopy "%BackDir%\foxyproxy.xml" %TempFolder%\ /y
+::localstore.rdf：工具列与视窗大小／位置的設定，有時刪掉可以解决一些介面上的问题。
+xcopy "%BackDir%\Profiles\localstore.rdf" %TempFolder%\Profiles\ /y
+::mimeTypes.rdf：下载特定类型的档案時要执行的動作。 可刪掉来还原原来下载的設定。
+xcopy "%BackDir%\Profiles\mimeTypes.rdf" %TempFolder%\Profiles\ /y
+::MyFirefox.7z：用於官方FX的便携設置。
+xcopy "%BackDir%\Profiles\MyFirefox.7z" %TempFolder%\Profiles\ /y
+::patternSubscriptions.json：FoxyProxy的訂閱列表設置。
+::xcopy "%BackDir%\patternSubscriptions.json" %TempFolder%\ /y
+::permissions.sqlite：存放特定网站是否可存取密码、cookies、弹出视窗、图片载入与附加元件……等权限的资料库。
+xcopy "%BackDir%\Profiles\permissions.sqlite" %TempFolder%\Profiles\ /y
+::persdict.dat：个人的拼字字典。
+xcopy "%BackDir%\Profiles\persdict.dat" %TempFolder%\Profiles\ /y
+::pluginreg.dat：用于plugin的MIME types。
+xcopy "%BackDir%\Profiles\pluginreg.dat" %TempFolder%\Profiles\ /y
+::Portable.7z：PCXFirefox的便携設置。
+xcopy "%BackDir%\Profiles\Portable.7z" %TempFolder%\Profiles\ /y
+::readme.txt：个人配置修改说明。
+xcopy "%BackDir%\Profiles\readme.txt" %TempFolder%\Profiles\ /y
+::stylish.sqlite：Stylish样式數据库。
+xcopy "%BackDir%\Profiles\stylish.sqlite" %TempFolder%\Profiles\ /y
+::user.js：使用者自订的設定，在这里的設定覆盖prefs.js的設定。
+xcopy "%BackDir%\Profiles\user.js" %TempFolder%\Profiles\ /y
+::xulstore.json：界面的一些状态。
+xcopy "%BackDir%\Profiles\xulstore.json" %TempFolder%\Profiles\ /y
+
+::讀取版本號和日期及時間
+::从批处理所在位置到Firefox程序文件夹（firefox），共跨了4层
+for /f "usebackq eol=; tokens=1,2 delims==" %%i in ("..\..\..\..\Firefox\application.ini")do (if %%i==Version set ver=%%j)
+::設置備份文件路徑以及文件名
+
+::完整日期和時間
+set tm1=%time:~0,2%
+set tm2=%time:~3,2%
+set tm3=%time:~6,2%
+set tm4=%time:~0,8%
+set da1=%date:~0,4%
+set da2=%date:~5,2%
+set da3=%date:~8,2%
+set ArchiveName=D:\CingFox_%da1%%da2%%da3%-%tm1%%tm2%%tm3%_%ver%.7z
+
+::小時數小于10点時的修正
+set /a tm1=%time:~0,2%*1
+if %tm1% LSS 10 set tm1=0%tm1%
+set ArchiveName=D:\CingFox_%da1%%da2%%da3%-%tm1%%tm2%%tm3%_%ver%.7z
+
+rem 開始備份
+7z.exe u -up1q3r2x2y2z2w2 %ArchiveName% "%TempFolder%"
+@echo 備份完成！并刪除臨時文件夾！
+rd "%TempFolder%" /s/q
+
+ECHO.&ECHO.Firefox完整包已打包完成，請按任意鍵 重啟Firefox 並退出！&PAUSE >NUL 2>NUL
+
+@ping 127.0.0.1>nul
+@start ..\..\..\..\Firefox\firefox.exe
+
+Goto end
+
+:Plugins-n-Software
+CLS
+MODE con: COLS=40 LINES=15
+ECHO.
+ECHO.
+ECHO    **********************************
+ECHO.
+ECHO              禁用索引服务
+ECHO.
+ECHO                1.执行
+ECHO.
+ECHO                2.返回
+ECHO.
+ECHO    **********************************
+ECHO.
+ECHO.
+Choice /C 12 /N /M 选择（1、2）：
+If ErrorLevel 1 If Not ErrorLevel 2 Goto Plugins-n-Software-1
+If ErrorLevel 2 If Not ErrorLevel 3 Goto menu
+
+:Plugins-n-Software-1
+MODE con: COLS=80 LINES=25
+Title 備份Plugins和Software文件夾 by Cing
+echo.
+echo    *** 備份Plugins和Software文件夾 ***
+echo.
+echo ============================================================
+echo    **注意：
+echo.
+echo    By Cing(Dupontjoy)
+echo.
+echo    按任意键继续……
+echo =============================================================
+pause>nul
+cls
+
+rem 設置備份路徑以及臨時文件夾
+cd /d %~dp0
+::从批处理所在位置到Plugins和Software文件夾，只跨了4层
+set BackDir=..\..\..\..\
+set TempFolder=..\..\..\..\Plugins-n-Software
+
+rem 复制目标文件到臨時文件夾
+
+::以下是文件夾
+::Plugins：外置便携插件
+xcopy "%BackDir%\Plugins" %TempFolder%\Plugins\  /s /y /i
+::Software：常用軟件
+xcopy "%BackDir%\Software" %TempFolder%\Software\  /s /y /i
+
+::需要刪除的项
+del %TempFolder%\Plugins\sumatrapdfcache\  /s /q 
+del %TempFolder%\Software\GFW\psiphon\psiphon3.exe.orig  /s /q 
+del %TempFolder%\Software\GFW\GoGoTester\gogo_cache  /s /q 
+
+::以下是文件
+::patternSubscriptions.json：FoxyProxy的訂閱列表設置。
+::xcopy "%BackDir%\patternSubscriptions.json" %TempFolder%\ /y
+
+::設置備份文件路徑以及文件名
+
+::完整日期和時間
+set tm1=%time:~0,2%
+set tm2=%time:~3,2%
+set tm3=%time:~6,2%
+set tm4=%time:~0,8%
+set da1=%date:~0,4%
+set da2=%date:~5,2%
+set da3=%date:~8,2%
+set ArchiveName=D:\Plugins-n-Software_%da1%%da2%%da3%-%tm1%%tm2%%tm3%.7z
+
+::小時數小于10点時的修正
+set /a tm1=%time:~0,2%*1
+if %tm1% LSS 10 set tm1=0%tm1%
+set ArchiveName=D:\Plugins-n-Software_%da1%%da2%%da3%-%tm1%%tm2%%tm3%.7z
+
+rem 開始備份
+7z.exe u -up1q3r2x2y2z2w2 %ArchiveName% "%TempFolder%"
+@echo 備份完成！并刪除臨時文件夾！
+rd "%TempFolder%" /s/q
+
+ECHO.&ECHO.Plugins和Software文件夾已打包完成，請按任意鍵退出！&PAUSE >NUL 2>NUL
+
+Goto end
+
+:Flash32
+CLS
+MODE con: COLS=40 LINES=15
+ECHO.
+ECHO.
+ECHO    **********************************
+ECHO.
+ECHO              禁用索引服务
+ECHO.
+ECHO                1.执行
+ECHO.
+ECHO                2.返回
+ECHO.
+ECHO    **********************************
+ECHO.
+ECHO.
+Choice /C 12 /N /M 选择（1、2）：
+If ErrorLevel 1 If Not ErrorLevel 2 Goto Flash32-1
+If ErrorLevel 2 If Not ErrorLevel 3 Goto menu
+
+:Flash32-1
+::color 2E
+MODE con: COLS=80 LINES=25
+Title 提取Flash32位插件 by Cing
+echo.
+echo    *** 提取Flash32位插件 ***
+echo.
+echo ============================================================
+echo    **注意：
+echo.
+echo    1.需要先安装非IE的Adobe Flash Player插件！
+echo.
+echo    2.本批处理用以提取32位插件，并打包
+echo.
+echo    3.如需提取64位Flash，请修改BackDir位置
+echo.
+echo    Edit By yndoc！
+echo.
+echo    Mod By Cing(Dupontjoy)
+echo.
+echo    按任意键继续……
+echo =============================================================
+pause>nul
+cls
+echo.
+echo   01、到官方下载非IE版Flash插件安装后提取！
+echo.
+echo   02、已经安装非IE版Flash插件的直接提取！
+echo.
+echo   03、退出此批处理。
+echo.
+echo.
+
+set /p id=请选择，按回车键执行（例如：07）:
+cls
+
+if "%id%"=="01" goto install
+if "%id%"=="02" goto set
+if "%id%"=="03" goto exit
+
+:install
+echo.
+echo    按任意键进入Flash官方下载地址……
+echo.
+pause>nul&start "" http://www.adobe.com/in/products/flashplayer/distribution3.html
+cls
+echo.
+echo    *请暂时不要关闭该批处理……
+echo.
+echo    *如果您已安装完毕Adobe Flash Player插件，请按任意键继续……
+pause>nul
+
+:set
+cd /d %~dp0
+set BackDir=C:\Windows\SysWOW64\Macromed\Flash
+set TempFolder=D:\Flash32
+
+::複製插件到臨時文件夾
+xcopy "%BackDir%\NPSWF32*.dll" %TempFolder%\  /s /y /i
+xcopy "%BackDir%\FlashPlayerPlugin*.exe" %TempFolder%\  /s /y /i
+xcopy "%BackDir%\plugin.vch" %TempFolder%\  /s /y /i
+
+::讀取版本號
+::找了好久，妙終於在這個回答找到了答案：http://zhidao.baidu.com/question/289963233.html
+for /f "delims=" %%i in ('dir /a-d /b "%BackDir%\NPSWF32*.dll"') do (set ver=%%i)
+echo %ver%
+
+::完整日期和時間
+set tm1=%time:~0,2%
+set tm2=%time:~3,2%
+set tm3=%time:~6,2%
+set tm4=%time:~0,8%
+set da1=%date:~0,4%
+set da2=%date:~5,2%
+set da3=%date:~8,2%
+set ArchiveName=D:\%ver%_%da1%%da2%%da3%-%tm1%%tm2%%tm3%.7z
+
+::小時數小于10点時的修正
+set /a tm1=%time:~0,2%*1
+if %tm1% LSS 10 set tm1=0%tm1%
+set ArchiveName=D:\%ver%_%da1%%da2%%da3%-%tm1%%tm2%%tm3%.7z
+
+rem 開始備份
+7z.exe u -up1q3r2x2y2z2w2 %ArchiveName% "%TempFolder%"
+@echo 備份完成！并刪除臨時文件夾！
+rd "%TempFolder%" /s/q
+
+ECHO.&ECHO.已打包完成，請按任意鍵退出，將跳轉到系統/控制面板/程序與功能！&PAUSE >NUL 2>NUL
+
+::跳轉到系統/控制面板/程序與功能
+appwiz.cpl
+rundll32.exe shell32.dll,Control_RunDLL appwiz.cpl
+
+Goto end
+
+:end
+CLS
+MODE con: COLS=40 LINES=15
+ECHO.
+ECHO.
+ECHO    **********************************
+ECHO.
+ECHO              禁用索引服务
+ECHO.
+ECHO                1.退出
+ECHO.
+ECHO                2.返回主菜單
+ECHO.
+ECHO    **********************************
+ECHO.
+ECHO.
+Choice /C 12 /N /M 选择（1、2）：
+If ErrorLevel 1 If Not ErrorLevel 2 Goto end-1
+If ErrorLevel 2 If Not ErrorLevel 3 Goto menu
+
+:end-1
+@exit
