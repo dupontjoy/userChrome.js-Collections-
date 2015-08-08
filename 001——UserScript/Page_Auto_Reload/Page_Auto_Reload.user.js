7f9cb3a3b20f8ee243eab49af5ace4318087d14d
玄清介 <dupontjoy@163.com>
2015-08-08 17:39:06 +0800

    08.08

--- 001——UserScript/Page_Auto_Reload/Page_Auto_Reload.user.js
+++ 001——UserScript/Page_Auto_Reload/Page_Auto_Reload.user.js
@@ -2,15 +2,16 @@
 // @name           Page Auto Reload
 // @description    Reload page on a given timing
 // @author         Cing
-// @include        http://ic.sjlpj.cn/*UpShelf/OperationManageList
-// @version        0.0.1
+// @include        http*://ic.sjlpj.cn/UpShelf/OperationManageList
+// @include        http*://ic.sjlpj.cn/#/UpShelf/OperationManageList
+// @version        0.0.2
 // @grant          none
 // @namespace      
 // ==/UserScript==
 
-var time = 3600000; //单位以毫秒计（1小時）
+var time = 600000; //单位以毫秒计（10分鐘）
 
 (function () 
 {
-setTimeout(function() {window.location.reload(true);}, time);
+setTimeout(function() {location.reload(true);}, time);
 })();
