3b181d22e26fb71833964917ca62cfe3e0086834
玄清介 <dupontjoy@163.com>
2015-08-15 16:45:47 +0800

    08.15

---
+++ BingDesktopThemeEveryDay/BingWallpaperDesktop.uc.js
@@ -0,0 +1,92 @@
+//==UserScript==
+// @name         BingWallpaperDesktop.uc.js
+// @description  每天第一次运行Firefox，将下载Bing首页背景，并设置为系统桌面背景。每天換背景，天天好心情，哦㖿！
+// @author       527836355
+// @Mod          chromeexe
+// @include      main
+// @charset      utf-8
+// @version      2.0
+// @downloadURL  https://raw.githubusercontent.com/dupontjoy/userChrome.js-Collections-/master/BingDesktopThemeEveryDay/BingWallpaperDesktop.uc.js
+// @homepageURL  https://github.com/dupontjoy/userChrome.js-Collections-/tree/master/BingDesktopThemeEveryDay
+
+// @note         2015.08.15 必应美图改到配置文件夹下
+
+//==/UserScript==
+
+
+var bingWallpaperDesktop=function(){
+	var prefManager=Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefBranch);
+	return{
+		fixDate:function(d){
+			if(d<10)
+				d='0'+d;
+			return d;
+		},
+		getNow:function(){
+			var d=new Date(),
+				dt=d.getDate()+1,
+				y,
+				m;
+			d.setDate(dt);
+			y=d.getFullYear();
+			m=this.fixDate(d.getMonth()+1);
+			dt=this.fixDate(d.getDate());
+			return parseInt(''+y+m+dt);
+		},
+		getDDate:function(){
+			var pref;
+			try{
+				pref=prefManager.getIntPref('extensions.bingwallpaperdesktop.datadate');
+				return pref;
+			}
+			catch(e){
+				prefManager.setIntPref('extensions.bingwallpaperdesktop.datadate',0);
+				return 0;
+			}
+		},
+		init:function(){
+			var ths=this;
+			var xhr=new XMLHttpRequest();
+			xhr.open('GET','http://cn.bing.com/HPImageArchive.aspx?format=js&idx=-1&n=1&nc='+new Date().getTime()+'&video=1',false);
+			xhr.onload=function(){
+				var response=JSON.parse(this.responseText),
+					images=response.images[0],
+					enddate=parseInt(images.enddate),
+					imgref=images.vid?'http://cn.bing.com'+images.vid.image:images.url,
+					cp=images.copyright,
+					ni=new Image();
+				if(ths.getDDate()<enddate){
+					prefManager.setIntPref('extensions.bingwallpaperdesktop.datadate',enddate);
+					ni.src=imgref;
+					ni.onload=function(){
+						var shell=Cc["@mozilla.org/browser/shell-service;1"].getService(Ci.nsIShellService);
+						shell.setDesktopBackground(ni,Ci.nsIShellService["BACKGROUND_STRETCH"]);
+						try{
+							var file=Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile),
+								path=Services.dirsvc.get("ProfD", Ci.nsILocalFile).path + "\\必应美图\\"+enddate+"-"+cp.replace(/ \(.*?\)/g,'')+".jpg";
+							file.initWithPath(path);
+							file.create(Components.interfaces.nsIFile.NOMAL_FILE_TYPE,0777);
+							Components.classes["@mozilla.org/embedding/browser/nsWebBrowserPersist;1"].createInstance(Components.interfaces.nsIWebBrowserPersist).saveURI(Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService).newURI(imgref,null,null),null,null,null,null,file,null);
+						}
+						catch(err){
+							return false;
+						}
+					};
+				}
+			};
+			xhr.send();
+		},
+		run:function(){
+			var ths=this,
+				runs=function(){
+					var now=ths.getNow(),
+						datadate=ths.getDDate();
+					if(now>datadate)
+						ths.init();
+				};
+			runs();
+			setInterval(runs,1000);
+		}
+	};
+}();
+bingWallpaperDesktop.run();
--- BingDesktopThemeEveryDay/readme.md
+++ BingDesktopThemeEveryDay/readme.md
@@ -7,3 +7,12 @@ by 527836355<br/>
 <p align="center"><img width="650" src="img/head.jpg"></p>
 
 <p align="center"><img width="650" src="img/preview.jpg"></p>
+
+——————————————————————
+Mod by chromeexe
+
+- 壁纸设置为必应第二天备用的那张
+- 返回数据是动态壁纸时取用的图不同
+- 如果浏览器运行期间跨越了凌晨零点，无需重启自动更换桌面壁纸
+
+from: http://tieba.baidu.com/p/2787211960
