078203662faa8da4e27d86f83ad8c249a8cf3230
玄清介 <dupontjoy@163.com>
2015-12-25 18:54:39 +0800

    12.25

--- One-click Start Software/每天启动(公司版).bat
+++ One-click Start Software/每天启动(公司版).bat
@@ -1,3 +1,9 @@
+::Processlaso特殊处理
+set dir4=D:\Program Files\System Tools\ProcessLassoPortable
+start "" "%dir4%\ProcessGovernor.exe" "/logfolder=%dir4%" "/configfolder=%dir4%"
+start "" "%dir4%\ProcessLasso.exe" "/logfolder=%dir4%" "/configfolder=%dir4%"
+
+::普通啟動
 start "" "D:\Program Files\BaiduYun\baiduyun.exe"
 start "" "D:\Program Files\360YunPan\360cloud\360Cloud.exe"
 start "" "D:\Program Files\Tencent\Foxmail\Foxmail.exe"
@@ -7,8 +13,3 @@ start "" "D:\Program Files\Thunder Network\Thunder\Program\Thunder.exe"
 start "" "C:\Program Files (x86)\XiaoMi\MiWiFi\MiWiFi.exe"
 start "" "D:\Program Files\Tencent\QQ\Bin\QQ.exe"
 start "" "D:\Program Files\Jingoal\Jingoal.exe"
-
-::Processlaso特殊处理
-set dir4=D:\Program Files\System Tools\ProcessLassoPortable
-start "" "%dir4%\ProcessGovernor.exe" "/logfolder=%dir4%" "/configfolder=%dir4%"
-start "" "%dir4%\ProcessLasso.exe" "/logfolder=%dir4%" "/configfolder=%dir4%"
