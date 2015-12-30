0d44d92fb926ece86fbd79a92cab365be086ef5b
玄清介 <dupontjoy@163.com>
2015-12-30 14:26:54 +0800

    12.30

--- One-click Start Software/每天启动(公司版).bat
+++ One-click Start Software/每天启动(公司版).bat
@@ -1,15 +1,17 @@
 ::Processlaso特殊处理
 set dir4=D:\Program Files\System Tools\ProcessLassoPortable
-start "" "%dir4%\ProcessGovernor.exe" "/logfolder=%dir4%" "/configfolder=%dir4%"
-start "" "%dir4%\ProcessLasso.exe" "/logfolder=%dir4%" "/configfolder=%dir4%"
+start "Processlaso" "%dir4%\ProcessGovernor.exe" "/logfolder=%dir4%" "/configfolder=%dir4%"
+start "Processlaso" "%dir4%\ProcessLasso.exe" "/logfolder=%dir4%" "/configfolder=%dir4%"
 
 ::普通啟動
-start "" "D:\Program Files\BaiduYun\baiduyun.exe"
-start "" "D:\Program Files\360YunPan\360cloud\360Cloud.exe"
-start "" "D:\Program Files\Tencent\Foxmail\Foxmail.exe"
-start "" "D:\Program Files\Mozilla Firefox\Software\Other\PicPick\picpick.exe"
-start "" "D:\Program Files\Mozilla Firefox\Software\GFW\Shadowsocks\Shadowsocks.exe"
-start "" "D:\Program Files\Thunder Network\Thunder\Program\Thunder.exe"
-start "" "C:\Program Files (x86)\XiaoMi\MiWiFi\MiWiFi.exe"
-start "" "D:\Program Files\Tencent\QQ\Bin\QQ.exe"
-start "" "D:\Program Files\Jingoal\Jingoal.exe"
+::start "百度雲同步" "D:\Program Files\BaiduYun\baiduyun.exe"
+start "360雲同步" "D:\Program Files\360YunPan\360cloud\360Cloud.exe"
+start "Foxmail" "D:\Program Files\Tencent\Foxmail\Foxmail.exe"
+start "PicPick" "D:\Program Files\Mozilla Firefox\Software\Other\PicPick\picpick.exe"
+start "SS" "D:\Program Files\Mozilla Firefox\Software\GFW\Shadowsocks\Shadowsocks.exe"
+start "迅雷" "D:\Program Files\Thunder Network\Thunder\Program\Thunder.exe"
+start "QQ" "D:\Program Files\Tencent\QQ\Bin\QQ.exe"
+
+::公司用軟件
+start "小米WiFi" "C:\Program Files (x86)\XiaoMi\MiWiFi\MiWiFi.exe"
+start "今目標" "D:\Program Files\Jingoal\Jingoal.exe"
