81620656a2e75e3ebea1ee19c7fb815237f11d84
玄清介 <dupontjoy@163.com>
2015-12-30 19:49:41 +0800

    12.30

--- One-click Start Software/每天启动(家庭版).bat
+++ One-click Start Software/每天启动(家庭版).bat
@@ -1,13 +1,14 @@
+::設置程序文件夾位置
+set dir=D:\Program Files
+
 ::Processlaso特殊处理
-set dir4=D:\Program Files\System Tools\ProcessLassoPortable
-start "Processlaso" "%dir4%\ProcessGovernor.exe" "/logfolder=%dir4%" "/configfolder=%dir4%"
-start "Processlaso" "%dir4%\ProcessLasso.exe" "/logfolder=%dir4%" "/configfolder=%dir4%"
+start "" "%dir%\System Tools\ProcessLassoPortable\ProcessGovernor.exe" "/logfolder=%dir%\System Tools\ProcessLassoPortable" "/configfolder=%dir%\System Tools\ProcessLassoPortable"
+start "" "%dir%\System Tools\ProcessLassoPortable\ProcessLasso.exe" "/logfolder=%dir%\System Tools\ProcessLassoPortable" "/configfolder=%dir%\System Tools\ProcessLassoPortable"
 
 ::普通啟動
-::start "百度雲同步" "D:\Program Files\BaiduYun\baiduyun.exe"
-start "360雲同步" "D:\Program Files\360YunPan\360cloud\360Cloud.exe"
-start "Foxmail" "D:\Program Files\Tencent\Foxmail\Foxmail.exe"
-start "PicPick" "D:\Program Files\Mozilla Firefox\Software\Other\PicPick\picpick.exe"
-start "SS" "D:\Program Files\Mozilla Firefox\Software\GFW\Shadowsocks\Shadowsocks.exe"
-start "迅雷" "D:\Program Files\Thunder Network\Thunder\Program\Thunder.exe"
-start "QQ" "D:\Program Files\Tencent\QQ\Bin\QQ.exe"
+start "" "%dir%\360YunPan\360cloud\360Cloud.exe"
+start "" "%dir%\Tencent\Foxmail\Foxmail.exe"
+start "" "%dir%\Mozilla Firefox\Software\Other\PicPick\picpick.exe"
+start "" "%dir%\Mozilla Firefox\Software\GFW\Shadowsocks\Shadowsocks.exe"
+start "" "%dir%\Thunder Network\Thunder\Program\Thunder.exe"
+start "" "%dir%\Tencent\QQ\Bin\QQ.exe"
