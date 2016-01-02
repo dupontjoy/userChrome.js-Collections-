e7508c7da3272b3e0ce932d3fbeaae27e2ff30ee
玄清介 <dupontjoy@163.com>
2016-01-02 16:47:57 +0800

    2016

--- One-click Start Software/每天启动(公司版).bat
+++ One-click Start Software/每天启动(公司版).bat
@@ -9,7 +9,7 @@ start "" "%dir%\System Tools\ProcessLassoPortable\ProcessLasso.exe" "/logfolder=
 ::普通啟動
 start "" "%dir%\360YunPan\360cloud\360Cloud.exe"
 start "" "%dir%\Tencent\Foxmail\Foxmail.exe"
-start "" "%dir%\Mozilla Firefox\Software\Other\PicPick\picpick.exe"
+start "" "%dir%\Mozilla Firefox\Software\Image\PicPick\picpick.exe"
 start "" "%dir%\Mozilla Firefox\Software\GFW\Shadowsocks\Shadowsocks.exe"
 start "" "%dir%\Thunder Network\Thunder\Program\Thunder.exe"
 start "" "%dir%\Tencent\QQ\Bin\QQ.exe"
