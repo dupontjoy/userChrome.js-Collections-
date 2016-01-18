04d36cd6747484d76463e5c9dd462f482ff9aacc
玄清介 <dupontjoy@163.com>
2016-01-19 07:43:51 +0800

    01.19

--- 1-Click Start Software/每天启动(公司版).bat
+++ 1-Click Start Software/每天启动(公司版).bat
@@ -4,17 +4,17 @@ set dir=D:\Program Files
 set dir2=C:\Program Files (x86)
 
 ::Processlaso特殊处理
-start "" "%dir%\System Tools\ProcessLassoPortable\ProcessGovernor.exe" "/logfolder=%dir%\System Tools\ProcessLassoPortable" "/configfolder=%dir%\System Tools\ProcessLassoPortable" & ping localhost -n 2
-start "" "%dir%\System Tools\ProcessLassoPortable\ProcessLasso.exe" "/logfolder=%dir%\System Tools\ProcessLassoPortable" "/configfolder=%dir%\System Tools\ProcessLassoPortable" & ping localhost -n 2
+start "" "%dir%\System Tools\ProcessLassoPortable\ProcessGovernor.exe" "/logfolder=%dir%\System Tools\ProcessLassoPortable" "/configfolder=%dir%\System Tools\ProcessLassoPortable" & ping localhost -n 5
+start "" "%dir%\System Tools\ProcessLassoPortable\ProcessLasso.exe" "/logfolder=%dir%\System Tools\ProcessLassoPortable" "/configfolder=%dir%\System Tools\ProcessLassoPortable" & ping localhost -n 5
 
 ::普通啟動
-start "" "%dir%\360YunPan\360cloud\360Cloud.exe" & ping localhost -n 2
-start "" "%dir%\Tencent\Foxmail\Foxmail.exe" & ping localhost -n 2
-start "" "%dir%\Mozilla Firefox\Software\Image\PicPick\picpick.exe" & ping localhost -n 2
-start "" "%dir%\Mozilla Firefox\Software\GFW\Shadowsocks\Shadowsocks.exe" & ping localhost -n 2
-start "" "%dir%\Thunder Network\Thunder\Program\Thunder.exe" & ping localhost -n 2
-start "" "%dir%\Tencent\QQ\Bin\QQ.exe" & ping localhost -n 2
+start "" "%dir%\360YunPan\360cloud\360Cloud.exe" & ping localhost -n 5
+start "" "%dir%\Tencent\Foxmail\Foxmail.exe" & ping localhost -n 5
+start "" "%dir%\Mozilla Firefox\Software\Image\PicPick\picpick.exe" & ping localhost -n 5
+start "" "%dir%\Mozilla Firefox\Software\GFW\Shadowsocks\Shadowsocks.exe" & ping localhost -n 5
+start "" "%dir%\Thunder Network\Thunder\Program\Thunder.exe" & ping localhost -n 5
+start "" "%dir%\Tencent\QQ\Bin\QQ.exe" & ping localhost -n 5
 
 ::公司用軟件
-start "" "%dir%\Jingoal\Jingoal.exe" & ping localhost -n 2
-start "" "%dir2%\XiaoMi\MiWiFi\MiWiFi.exe" & ping localhost -n 2
+start "" "%dir%\Jingoal\Jingoal.exe" & ping localhost -n 5
+start "" "%dir2%\XiaoMi\MiWiFi\MiWiFi.exe" & ping localhost -n 5
--- 1-Click Start Software/每天启动(家庭版).bat
+++ 1-Click Start Software/每天启动(家庭版).bat
@@ -3,13 +3,13 @@
 set dir=D:\Program Files
 
 ::Processlaso特殊处理
-start "" "%dir%\System Tools\ProcessLassoPortable\ProcessGovernor.exe" "/logfolder=%dir%\System Tools\ProcessLassoPortable" "/configfolder=%dir%\System Tools\ProcessLassoPortable" & ping localhost -n 2
-start "" "%dir%\System Tools\ProcessLassoPortable\ProcessLasso.exe" "/logfolder=%dir%\System Tools\ProcessLassoPortable" "/configfolder=%dir%\System Tools\ProcessLassoPortable" & ping localhost -n 2
+start "" "%dir%\System Tools\ProcessLassoPortable\ProcessGovernor.exe" "/logfolder=%dir%\System Tools\ProcessLassoPortable" "/configfolder=%dir%\System Tools\ProcessLassoPortable" & ping localhost -n 5
+start "" "%dir%\System Tools\ProcessLassoPortable\ProcessLasso.exe" "/logfolder=%dir%\System Tools\ProcessLassoPortable" "/configfolder=%dir%\System Tools\ProcessLassoPortable" & ping localhost -n 5
 
 ::普通啟動
-start "" "%dir%\360YunPan\360cloud\360Cloud.exe" & ping localhost -n 2
-start "" "%dir%\Tencent\Foxmail\Foxmail.exe" & ping localhost -n 2
-start "" "%dir%\Mozilla Firefox\Software\Image\PicPick\picpick.exe" & ping localhost -n 2
-start "" "%dir%\Mozilla Firefox\Software\GFW\Shadowsocks\Shadowsocks.exe" & ping localhost -n 2
-start "" "%dir%\Thunder Network\Thunder\Program\Thunder.exe" & ping localhost -n 2
-start "" "%dir%\Tencent\QQ\Bin\QQ.exe" & ping localhost -n 2
+start "" "%dir%\360YunPan\360cloud\360Cloud.exe" & ping localhost -n 5
+start "" "%dir%\Tencent\Foxmail\Foxmail.exe" & ping localhost -n 5
+start "" "%dir%\Mozilla Firefox\Software\Image\PicPick\picpick.exe" & ping localhost -n 5
+start "" "%dir%\Mozilla Firefox\Software\GFW\Shadowsocks\Shadowsocks.exe" & ping localhost -n 5
+start "" "%dir%\Thunder Network\Thunder\Program\Thunder.exe" & ping localhost -n 5
+start "" "%dir%\Tencent\QQ\Bin\QQ.exe" & ping localhost -n 5
