d7bc1620dcaf122d0e624bdf8cbff3a666ad9dae
玄清介 <dupontjoy@163.com>
2016-01-18 08:28:23 +0800

    01.18

--- 1-Click Start Software/每天启动(家庭版).bat
+++ 1-Click Start Software/每天启动(家庭版).bat
@@ -1,14 +1,15 @@
+::2016.01.18
 ::設置程序文件夾位置
 set dir=D:\Program Files
 
 ::Processlaso特殊处理
-start "" "%dir%\System Tools\ProcessLassoPortable\ProcessGovernor.exe" "/logfolder=%dir%\System Tools\ProcessLassoPortable" "/configfolder=%dir%\System Tools\ProcessLassoPortable"
-start "" "%dir%\System Tools\ProcessLassoPortable\ProcessLasso.exe" "/logfolder=%dir%\System Tools\ProcessLassoPortable" "/configfolder=%dir%\System Tools\ProcessLassoPortable"
+start "" "%dir%\System Tools\ProcessLassoPortable\ProcessGovernor.exe" "/logfolder=%dir%\System Tools\ProcessLassoPortable" "/configfolder=%dir%\System Tools\ProcessLassoPortable" & ping localhost -n 2
+start "" "%dir%\System Tools\ProcessLassoPortable\ProcessLasso.exe" "/logfolder=%dir%\System Tools\ProcessLassoPortable" "/configfolder=%dir%\System Tools\ProcessLassoPortable" & ping localhost -n 2
 
 ::普通啟動
-start "" "%dir%\360YunPan\360cloud\360Cloud.exe"
-start "" "%dir%\Tencent\Foxmail\Foxmail.exe"
-start "" "%dir%\Mozilla Firefox\Software\Image\PicPick\picpick.exe"
-start "" "%dir%\Mozilla Firefox\Software\GFW\Shadowsocks\Shadowsocks.exe"
-start "" "%dir%\Thunder Network\Thunder\Program\Thunder.exe"
-start "" "%dir%\Tencent\QQ\Bin\QQ.exe"
+start "" "%dir%\360YunPan\360cloud\360Cloud.exe" & ping localhost -n 2
+start "" "%dir%\Tencent\Foxmail\Foxmail.exe" & ping localhost -n 2
+start "" "%dir%\Mozilla Firefox\Software\Image\PicPick\picpick.exe" & ping localhost -n 2
+start "" "%dir%\Mozilla Firefox\Software\GFW\Shadowsocks\Shadowsocks.exe" & ping localhost -n 2
+start "" "%dir%\Thunder Network\Thunder\Program\Thunder.exe" & ping localhost -n 2
+start "" "%dir%\Tencent\QQ\Bin\QQ.exe" & ping localhost -n 2
