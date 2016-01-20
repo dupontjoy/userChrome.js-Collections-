419549306c5f6526d633431bdda874972efb68ac
玄清介 <dupontjoy@163.com>
2016-01-20 20:29:24 +0800

    01.20

--- 1-Click Start Software/每天启动(公司版).bat
+++ 1-Click Start Software/每天启动(公司版).bat
@@ -1,4 +1,12 @@
 ::2016.01.20
+
+::最小化运行批处理
+::From: http://www.jb51.net/article/7347.htm
+@echo off
+if "%1"=="h" goto begin
+start mshta vbscript:createobject("wscript.shell").run("""%~nx0"" h",0)(window.close)&&exit
+:begin
+
 ::設置程序文件夾位置
 set dir=D:\Program Files
 set dir2=C:\Program Files (x86)
