7c1b345a24793ec416e9891bdbc1e6aac06a6166
玄清介 <dupontjoy@163.com>
2016-01-21 09:32:12 +0800

    01.21

--- zzz.UserScript/Weixin_Image/Weixun_Image.user.js
+++ zzz.UserScript/Weixin_Image/Weixun_Image.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name        Weixin_Image
 // @namespace   微信图片反盗链
-// @description 微信图片反盗链
+// @description 设置data-src等于src. 需要配合Referchange使用, 设置qpic.cn和qlogo.cn为@Block.
 // @include     *
-// @version     2016.01.17
+// @version     2016.01.21
 // @grant 		  none
 // ==/UserScript==
 (function(){
