fa7ddc31c73bb6d0779d5b9d6e3783e55f56730b
玄清介 <dupontjoy@163.com>
2016-01-30 09:39:21 +0800

    01.30

--- 1-Click Start Software/每天启动(公司版).bat
+++ 1-Click Start Software/每天启动(公司版).bat
@@ -15,12 +15,12 @@ start "" "%dir%\System Tools\ProcessLassoPortable\ProcessGovernor.exe" "/logfold
 start "" "%dir%\System Tools\ProcessLassoPortable\ProcessLasso.exe" "/logfolder=%dir%\System Tools\ProcessLassoPortable" "/configfolder=%dir%\System Tools\ProcessLassoPortable" & ping localhost -n 5
 
 ::公司用軟件
-start "" "%dir%\Jingoal\Jingoal.exe" & ping localhost -n 20
-start "" "%dir%\Adobe\Photoshop\Photoshop\photoshop.exe" & ping localhost -n 20
+start "" "%dir%\Jingoal\Jingoal.exe" & ping localhost -n 30
+start "" "%dir%\Adobe\Photoshop\Photoshop\photoshop.exe" & ping localhost -n 30
 start "" "%dir%\XiaoMi\MiWiFi\MiWiFi.exe" & ping localhost -n 5
 
 ::普通啟動
-start "" "%dir%\Tencent\QQ\Bin\QQ.exe" & ping localhost -n 20
+start "" "%dir%\Tencent\QQ\Bin\QQ.exe" & ping localhost -n 30
 start "" "%dir%\Thunder Network\Thunder\Program\Thunder.exe" & ping localhost -n 5
 start "" "%dir%\Tencent\Foxmail\Foxmail.exe" & ping localhost -n 5
 start "" "%dir%\360YunPan\360cloud\360Cloud.exe" & ping localhost -n 5
