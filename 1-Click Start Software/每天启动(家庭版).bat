39f4d0167052a4b8db3320a7523a0762ee931a69
玄清介 <dupontjoy@163.com>
2016-05-29 16:38:44 +0800

    05.29

--- 1-Click Start Software/每天启动(家庭版).bat
+++ 1-Click Start Software/每天启动(家庭版).bat
@@ -1,4 +1,4 @@
-::2016.04.27
+::2016.05.29
 
 ::最小化运行批处理
 ::From: http://www.jb51.net/article/7347.htm
@@ -16,6 +16,7 @@ start "" "%dir%\System Tools\ProcessLassoPortable\ProcessLasso.exe" "/configfold
 
 ::普通啟動
 ::start "" "%dir%\BingDesktop2\BingDesktop.exe"
+start "" "%dir%\finalspeedclient\finalspeedclient.exe"
 start "" "%dir%\Tencent\QQ\Bin\QQ.exe" & ping localhost -n 3
 start "" "%dir%\Tencent\Foxmail\Foxmail.exe" /min
 start "" "%dir%\BaiduYun\baiduyun.exe"
