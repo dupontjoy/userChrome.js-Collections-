e4197c180f44106731d9a92e913a3cc89b835e81
玄清介 <dupontjoy@163.com>
2016-06-28 18:01:48 +0800

    06.28

---
+++ zzz.UserScript/Discuz_relatelink_bug/Discuz_relatelink_bug.user.js
@@ -0,0 +1,19 @@
+﻿// ==UserScript==
+// @name        修正 Discuz 论坛 relatedlink 的 bug
+// @namespace   Y大 (http://bbs.kafan.cn/forum.php?mod=redirect&goto=findpost&ptid=1755690&pid=32037181)
+// @include     *forum.php?mod=viewthread&tid=*
+// @version     1
+// @grant       none
+// @run-at      document-start
+// ==/UserScript==
+ 
+function run() {
+  Object.defineProperty(window, 'relatedlink', {
+    get: function() { return []; },
+    set: function() {}
+  });
+}
+ 
+var script = document.createElement('script');
+script.textContent = '(' + run.toString() + ')();';
+document.head.appendChild(script)
