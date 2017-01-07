33d055af7626d7f76454a8019010303b813cf370
玄清介 <dupontjoy@163.com>
2017-01-07 09:31:46 +0800

    01.07

--- 1-Click Start Software/QD-Work.bat
+++ 1-Click Start Software/QD-Work.bat
@@ -1,4 +1,4 @@
-::2016.10.11
+::2017.01.07
 
 @echo off
 
@@ -10,6 +10,7 @@
 
 ::設置程序文件夾位置
 set dir=D:\Program Files
+set dir2=C:\Program Files (x86)
 
 ::公司用軟件
 start "" "%dir%\Tencent\Foxmail\Foxmail.exe" /min
@@ -17,6 +18,8 @@ start "" "%dir%\CingFox\Software\GFW\Shadowsocks\ShadowsocksR-dotnet4.0.exe"
 ::start "" "%dir%\CingFox\Software\GFW\Shadowsocks\SSCap64\SSCap.exe"
 start "" "%dir%\Tencent\WeChat\WeChat.exe" & ping localhost -n 3
 start "" "%dir%\Jingoal\Jingoal.exe"
+
+start "" "%dir2%\XiaoMi\MiWiFi\MiWiFi.exe"
 
 ::完成後退出
 exit
