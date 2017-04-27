ebc6960441fce4a3ac631fdb047f6ea957b10bd9
玄清介 <dupontjoy@163.com>
2017-04-27 10:49:54 +0800

    04.27

--- zzz.UserScript/Weixin_Image/图片源链接更改.user.js
+++ zzz.UserScript/Weixin_Image/图片源链接更改.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @name        Weixin_Image
-// @namespace   微信图片反盗链
+// @name        图片源链接更改
+// @namespace   微信, 得到图片反盗链
 // @description 设置data-src等于src. 需要配合Referchange使用, 设置qpic.cn和qlogo.cn为@Block.
 // @include     *
-// @version     2016.01.21
+// @version     2017.04.27
 // @grant 		  none
 // ==/UserScript==
 (function(){
 var wxIMGs=document.querySelectorAll('img[data-src][data-original]');for(i=0;i<wxIMGs.length;i++){wxIMGs[i].src=wxIMGs[i].getAttribute('data-src');}void(0)
+var dedaoIMGs=document.querySelectorAll('img[src][data-url]');for(i=0;i<dedaoIMGs.length;i++){dedaoIMGs[i].src=dedaoIMGs[i].getAttribute('data-url');}void(0)
 })()
