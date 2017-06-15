7b37a5a3d24946852835ab05646ae43874dee5f8
玄清介 <dupontjoy@163.com>
2017-06-15 22:41:58 +0800

    06.15

--- 1-Click Start Software/QD-Common.bat
+++ 1-Click Start Software/QD-Common.bat
@@ -25,8 +25,8 @@ start "" "%dir%\CingFox\Software\Listary Pro\listary.exe"
 start "" "%dir%\Nutstore\Nutstore.exe" /min
 start "" "%dir%\CingFox\Software\picpick\picpick.exe"
 start "" "%dir%\Tencent\QQ\Bin\QQ.exe"
-start "" "%dir%\CingFox\Software\GFW\MEOW\MEOW.exe"
-::start "" "%dir%\CingFox\Software\GFW\Shadowsocks\ShadowsocksR-dotnet4.0.exe"
+::start "" "%dir%\CingFox\Software\GFW\MEOW\MEOW.exe"
+start "" "%dir%\CingFox\Software\GFW\Shadowsocks\ShadowsocksR-dotnet4.0.exe"
 
 ::完成後退出
 exit
