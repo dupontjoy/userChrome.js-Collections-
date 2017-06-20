34302fab57cf373088d208a32b4b2a413fcb8dd7
玄清介 <dupontjoy@163.com>
2017-06-20 11:33:21 +0800

    06.20

--- Tabplus Modc/TabPlus_ModC.uc.js
+++ Tabplus Modc/TabPlus_ModC.uc.js
@@ -1,10 +1,8 @@
 // ==UserScript==
 // @description  Tab Plus 标签页增强
 // @include      chrome://browser/content/browser.xul
+// @version      2017.06.20
 
-// 2015.01.23  新标签打开『查看图片』
-// 2014.07.26  點擊頁面恢復原來的地址
-// 2014.05.14  修改激活左侧标签
 // ==/UserScript==
 
 /*=====以下爲另外收集整合的腳本=====*/
@@ -26,10 +24,12 @@
 
 })();
 
-// 关闭当前标签页回到左边标签
-try {
-eval("gBrowser._blurTab = " + gBrowser._blurTab.toString().replace('this.selectedTab = tab;', "this.selectedTab = aTab.previousSibling? aTab.previousSibling : tab;"));
-}catch(e){};
+//关闭当前标签页回到左边标签，firefox 47+失效，改用tabclose事件
+	try {
+		eval("gBrowser._blurTab = " + gBrowser._blurTab.toString()
+			.replace('this.selectedTab = tab;',
+			"this.selectedTab = aTab.previousSibling || tab;"));
+	} catch (e) {}
 
 //點擊頁面恢復原來的地址
 gBrowser.addEventListener("DOMWindowCreated", function () {
