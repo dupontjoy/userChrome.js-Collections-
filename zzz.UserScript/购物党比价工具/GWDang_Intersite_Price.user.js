1e875d8e3bab331b6703bb133f42556b4a85b9f8
玄清介 <dupontjoy@163.com>
2017-06-27 17:26:29 +0800

    06.27

--- zzz.UserScript/购物党比价工具/GWDang_Intersite_Price.user.js
+++ zzz.UserScript/购物党比价工具/GWDang_Intersite_Price.user.js
@@ -1,22 +1,11 @@
 ﻿// ==UserScript==
 // @name        GWDang Intersite Price
 // @namespace   购物党比价工具
-// @description 京东.淘宝.天猫.易迅.亚马逊中国.一号店.苏宁易购.当当网.国美在线.新蛋网中国.拍拍网自动比价
-// @include     http*://item.jd.com/*
-// @include     http*://*.taobao.com/*
-// @include     http*://*.tmall.com/*
-// @include     http*://*.yixun.com/*
-// @include     http*://*.51buy.com/*
-// @include     http*://*.amazon.cn/*
-// @include     http*://*.yhd.com/*
-// @include     http*://*.suning.com/*
-// @include     http*://*.dangdang.com/*
-// @include     http*://*.gome.com.cn/*
-// @include     http*://*.newegg.cn/*
-// @include     http*://*.paipai.com/*
-// @version     1.0-20160116
+// @description 京东.淘宝.天猫.易迅.亚马逊中国.一号店.苏宁易购.当当网.国美在线.新蛋网中国.拍拍网自动比价(实际支持网站超过700个)
+// @include     *
+// @version     1.0-2017.06.27
 // @grant 		  none
-// @require     http://www.gwdang.com/get.js?f=/js/gwdang_extension.js
+// @require     https://greasyfork.org/scripts/14464-gwd/code/gwd.js
 // ==/UserScript==
 (function(){
 var s = document.createElement('script');s.setAttribute('src','@require');document.body.appendChild(s);
