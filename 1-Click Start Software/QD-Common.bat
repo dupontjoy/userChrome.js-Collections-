ecd0a17539854e0f3ff67aa1248473e65535d721
玄清介 <dupontjoy@163.com>
2017-08-15 11:05:22 +0800

    08.15

--- 1-Click Start Software/QD-Common.bat
+++ 1-Click Start Software/QD-Common.bat
@@ -26,8 +26,9 @@ start "" "%dir%\Nutstore\Nutstore.exe" /min
 start "" "%dir%\CingFox\Software\picpick\picpick.exe"
 start "" "%dir%\Tencent\QQ\Bin\QQ.exe"
 ::start "" "%dir%\Tencent\Tim\Bin\Tim.exe"
-start "" "%dir%\CingFox\Software\GFW\SSR\ShadowsocksR-dotnet4.0.exe"
-start "" "%dir%\CingFox\Software\GFW\MEOW\MEOW.exe"
+::start "" "%dir%\CingFox\Software\GFW\SSR\ShadowsocksR-dotnet4.0.exe"
+::start "" "%dir%\CingFox\Software\GFW\MEOW\MEOW.exe"
+start "" "%dir%\CingFox\Software\GFW\SocksCap64\SocksCap64.exe"
 
 ::完成後退出
 exit
