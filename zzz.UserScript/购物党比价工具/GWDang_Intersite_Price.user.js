07f0f74640dd794fd06caf091957a2d2dd774cef
玄清介 <dupontjoy@163.com>
2017-09-22 17:08:22 +0800

    09.22

--- zzz.UserScript/购物党比价工具/GWDang_Intersite_Price.user.js
+++ zzz.UserScript/购物党比价工具/GWDang_Intersite_Price.user.js
@@ -3,9 +3,9 @@
 // @namespace   购物党比价工具
 // @description 京东.淘宝.天猫.易迅.亚马逊中国.一号店.苏宁易购.当当网.国美在线.新蛋网中国.拍拍网自动比价(实际支持网站超过700个)
 // @include     *
-// @version     1.0-2017.06.27
+// @version     1.0-2017.09.22
 // @grant 		  none
-// @require     https://greasyfork.org/scripts/14464-gwd/code/gwd.js
+// @require     https://browser.gwdang.com/get.js?f=/js/gwdang_extension.js
 // ==/UserScript==
 (function(){
 var s = document.createElement('script');s.setAttribute('src','@require');document.body.appendChild(s);
