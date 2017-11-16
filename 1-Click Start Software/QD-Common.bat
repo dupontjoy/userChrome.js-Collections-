d64f69e5a8e329cf9085a80cdb0c21bc131d0978
玄清介 <dupontjoy@163.com>
2017-11-16 09:25:59 +0800

    11.16

--- 1-Click Start Software/QD-Common.bat
+++ 1-Click Start Software/QD-Common.bat
@@ -26,7 +26,6 @@ start "" "%dir%\Nutstore\Nutstore.exe" /min
 start "" "%dir%\CingFox\Software\picpick\picpick.exe"
 start "" "%dir%\Tencent\QQ\Bin\QQ.exe"
 start "" "%dir%\CingFox\Software\GFW\SSR\ShadowsocksR-dotnet4.0.exe"
-start "" "%dir%\CingFox\Software\Ditto\Ditto.exe"  /min
 
 ::完成後退出
 exit
